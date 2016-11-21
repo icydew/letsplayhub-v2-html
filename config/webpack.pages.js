430618b441554c9f0ca30132072d568c9d248d93
icydew <icydew@live.com>
2016-11-21 20:14:48 +0200

    base channel and game pages

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -16,9 +16,26 @@ module.exports = {
       template: './app/channels.ejs'
     }),
     new HtmlWebpackPlugin({
-      filename: 'about.html',
-      template: './app/about.html'
+      filename: 'game-all-video.html',
+      template: './app/game-all-video.ejs'
     }),
+    new HtmlWebpackPlugin({
+      filename: 'game-all-channels.html',
+      template: './app/game-all-channels.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'channel-all-video.html',
+      template: './app/channel-all-video.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'channel-all-games.html',
+      template: './app/channel-all-games.ejs'
+    }),
+
+
+
+
+
     new HtmlWebpackPlugin({
       filename: 'templated.html',
       template: './app/templated.ejs',
